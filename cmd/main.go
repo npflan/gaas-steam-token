8ea2165ad42fce167133a77ec028304b2fb7d948
Jesper Petersen <jesper.petersen@se.com>
2020-10-09 22:11:56 +0200

    GRPC final

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	_ "./proto"
+	handler "github.com/Cytram/csgo-token/pkg"
+	pb "github.com/Cytram/csgo-token/proto"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
-
 )
 
 func main() {
-	// lis, err := net.Listen("tcp", "8080")
-	// if err != nil {
-	// 	log.Fatalf("failed to listen: %v", err)
-	// }
-	// s := grpc.NewServer()
-	// pb.RegisterTokenserviceServer(s, &)
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterTokenserviceServer(s, &handler.Server{})
 
-	// if err := s.Serve(lis); err != nil {
-	// 	log.Fatalf("failed to serve: %v", err)
-	// }
-
-
-
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.2
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	google.golang.org/grpc v1.33.0
 	google.golang.org/protobuf v1.25.0
 )

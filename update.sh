06dee6586f29bd9a479e62cd908adff363de1a18
Jesper Petersen <jesper.petersen@se.com>
2020-10-10 17:53:28 +0200

    20201010-175259

--- update.sh
+++ update.sh
@@ -3,16 +3,19 @@
 ./build_proto.sh
 
 VERSION=$(date +%Y%m%d-%H%M%S)
+echo $VERSION
 # Local registry
+echo "Pushing images to local registry"
 docker build -t registry.npf.dk/steam-token-handler .
-docker tag registry.npf.dk/steam-token-handler registry.npf.dk/steam-token-handler:$version
-docker push registry.npf.dk/steam-token-handler:$version
-docker push registry.npf.dk/steam-token-handler
+docker tag registry.npf.dk/steam-token-handler registry.npf.dk/steam-token-handler:$VERSION
+docker push registry.npf.dk/steam-token-handler:$VERSION
+docker push registry.npf.dk/steam-token-handler:latest
 
 # Docker Hub
-docker tag registry.npf.dk/steam-token-handler npflan/gaas-steam-token:$version
+echo "Pushing images to Docker hub"
+docker tag registry.npf.dk/steam-token-handler npflan/gaas-steam-token:$VERSION
 docker tag registry.npf.dk/steam-token-handler npflan/gaas-steam-token
-docker push npflan/gaas-steam-token:$version
-docker push npflan/gaas-steam-token
+docker push npflan/gaas-steam-token:$VERSION
+docker push npflan/gaas-steam-token:latest
 
 cd manifests && kustomize edit set image registry.npf.dk/steam-token-handler=registry.npf.dk/steam-token-handler:$VERSION
